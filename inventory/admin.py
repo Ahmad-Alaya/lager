cb26ed6a491716722240b13f71d52e4dfefe0aba
ahmad.alaya <alaya@next-kraftwerke.de>
2023-08-04 13:20:36 +0200

    Add Backofen class

--- inventory/admin.py
+++ inventory/admin.py
@@ -6,37 +6,40 @@ basic_field=['marke', 'anzahl','preis','']
 
 class WaschmaschineAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'fassung', 'preis',]
-    list_display = ['marke', 'fassung', 'anzahl', 'model','toploader','preis']
+    list_display = ['marke', 'fassung', 'anzahl', 'model','toploader','preis','farbe','note']
 
 
 class SpuelmaschineAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'breite', 'art']
-    list_display = ["marke", "breite", 'art', 'anzahl',  'model', 'preis']
+    list_display = ["marke", "breite", 'art', 'anzahl',  'model', 'preis','farbe','note']
 
 class KuehlschrankAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'type']
-    list_display = ['marke', 'type', 'anzahl',  'model', 'preis']
+    list_display = ['marke', 'type', 'anzahl',  'model', 'preis','farbe','note']
 
 class HerdsetAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'type',]
-    list_display = ['marke', 'anzahl',  'model', 'preis','induktion','pyrolyse', 'Anzahl_kochfelder','umluft']
+    list_display = ['marke', 'anzahl',  'model', 'preis','induktion','pyrolyse', 'Anzahl_kochfelder','umluft','farbe','note']
 
 class HerdplatteAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'type',]
-    list_display = ['marke', 'breite', 'anzahl',  'model', 'preis','induktion', 'Anzahl_kochfelder']
+    list_display = ['marke', 'breite', 'anzahl',  'model', 'preis','induktion', 'Anzahl_kochfelder','note']
 
 class StandherdAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'type',]
-    list_display = ['marke', 'anzahl',  'model', 'preis','induktion','pyrolyse', 'Anzahl_kochfelder','umluft']
+    list_display = ['marke', 'anzahl',  'model', 'preis','induktion','pyrolyse', 'Anzahl_kochfelder','umluft','farbe','note']
 
+class BackofenAdmin(admin.ModelAdmin):
+    search_fields = ['marke', 'model', 'type',]
+    list_display = ['marke', 'anzahl',  'model', 'preis', 'pyrolyse', 'umluft','farbe','note']
 
 class TrocknerAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'fassung', 'preis',]
-    list_display = ['marke', 'fassung', 'anzahl', 'model','art','preis']
+    list_display = ['marke', 'fassung', 'anzahl', 'model','art','preis','note']
 
 class AbzughaubeAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'fassung', 'preis',]
-    list_display = ['marke', 'anzahl', 'model','art','preis', 'breite']
+    list_display = ['marke', 'anzahl', 'model','art','preis', 'breite','farbe','note']
 
 class SonstAdmin(admin.ModelAdmin):
     search_fields = ['marke', 'model', 'fassung', 'preis',]
@@ -56,6 +59,7 @@ admin.site.register(Spuelmaschine,SpuelmaschineAdmin)
 admin.site.register(Herdset,HerdsetAdmin)
 admin.site.register(Herdplatte,HerdplatteAdmin)
 admin.site.register(Standherd,StandherdAdmin)
+admin.site.register(Backofen,BackofenAdmin)
 admin.site.register(Trockner,TrocknerAdmin)
 admin.site.register(Abzughaube,AbzughaubeAdmin)
 admin.site.register(Sonst,SonstAdmin)
